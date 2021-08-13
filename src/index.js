0c5709c8e6a26628efecb7939fe1953a086a666b
Aladdelin <aladdelinxr@gmail.com>
2021-08-13 19:13:18 +0300

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  return "one";
-}
+    const arr3 = ["zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "];
+    const arr2 = ["", "", "twenty ", "thirty ", "fourty ", "fivety ", "sixty ", "seventy ", "eighty ", "ninety "];
+    const arr1 = ["", "one hundred ", "two hundred ", "three hundred ", "four hundred ", "five hundred ", "six hundred ", "seven hundred ", "eight hundred ", "nine hundred "];
+    let result = number.toString();
+    result = result.split("").map((elem) => +elem);
+    let fullResult = arr1[result[0]] + arr2[result[1]] + arr3[result[2]];
+    return fullResult;
+};
